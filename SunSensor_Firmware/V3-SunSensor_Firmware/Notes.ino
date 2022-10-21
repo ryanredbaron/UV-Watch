657e168dad21b0400d69ddc360f0a9c797093062
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-10-21 09:22:57 -0700

    Merging Mark's Firmware

---
+++ SunSensor_Firmware/V3-SunSensor_Firmware/Notes.ino
@@ -0,0 +1,11 @@
+// Todo (for Ryan)
+//
+
+// Todo (for Mark)
+//
+//  Battery interface
+//  Accel XYZ and tap
+//  Vibe hookup - make PCB ????
+//  Relieve case so it works reliably for programming
+//  get Eye2See running
+//  Test VEML interface
